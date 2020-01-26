fdb4850e4861f7797e3eb3a6e50aef4b8b0e35fd
shilpa <shilpasantoshjadhao9011@gmail.com>
2020-01-26 13:42:32 +0530

    for pavanrequired this file thats ya

---
+++ smita practice sets.py
@@ -0,0 +1,17 @@
+s={1,2,3,4}
+##print(s)
+###s.pop()
+##s.update([6,7,8])
+##print(s)
+
+dict={1:34,2:45,5:65}
+dict[2]=50
+print(dict)
+dict[3]=78
+print(dict)
+##s1={4,7,8,9,10}
+##print(s|s1)
+##print(s&s1)
+##print(s-s1)
+
+
